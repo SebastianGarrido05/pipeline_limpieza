de16bd0cce9a54eb499c9e63805c27b6df67fc95
SebastianGarrido05 <se.garridom@duocuc.cl>
2026-05-22 17:52:29 -0400

    Se crea la funcion final con loggin implementado
    funciones creadas anteriormente son descartadas
    
    Funcion procesar_csv se le añade
    Rango peso solicitado en el ppt
    Dummies para que quede formato "especie_(especie correspondiente)"
    Años cliente solicitado en el ppt
    
    Formato fecha queda igual y solo se traspasa a la nueva funcion
    
    Loggin finalizado con mensajes implementados
    
    Se crea archivo resultado (datos_final.csv) de la funcion 'procesar_csv'

--- funciones.py
+++ funciones.py
@@ -1,27 +1,171 @@
 import pandas as pd
+import logging
+from datetime import datetime
+
+# login
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+
+def procesar_csv(file_path, output_path):
+
+    logging.info("Leyendo CSV...")
+    df = pd.read_csv(file_path)
+
+    logging.info("Normalizando texto...")
+    df = df.map(lambda x: x.lower().strip() if isinstance(x, str) else x)
+
+    logging.info("Ordenando por id_mascota...")
+    df = df.sort_values(by='id_mascota')
+
+    logging.info("Eliminando filas completamente vacías...")
+    df = df.dropna(
+        subset=[
+            'nombre','especie','raza','edad_años',
+            'peso_kg','fecha_consulta','dueño_nombre',
+            'dueño_email','motivo_consulta','costo_consulta'
+        ],
+        how='all'
+    )
+
+    logging.info("Rellenando datos faltantes...")
+    df = df.fillna('No_Info')
+
+    logging.info("Normalizando especies...")
+    df['especie'] = df['especie'].replace({
+        'cat': 'gato',
+        'gata': 'gato',
+        'dog': 'perro',
+        'perra': 'perro'
+    })
+
+    logging.info("Eliminando duplicados...")
+    df = df.drop_duplicates(subset='dueño_email')
+
+    # RANGO PESO
+
+    logging.info("Creando columna rango_peso...")
+
+    def clasificar_peso(peso):
+
+        try:
+            peso = float(peso)
+
+            if peso < 4:
+                return 'bajo'
+
+            elif peso < 15:
+                return 'normal'
+
+            elif peso < 40:
+                return 'alto'
+
+            else:
+                return 'obeso'
+
+        except:
+            return 'No_Info'
+
+    df['rango_peso'] = df['peso_kg'].apply(clasificar_peso)
+
+
+    # GET DUMMIES
+
+    logging.info("Codificando especie con get_dummies...")
+
+    dummies = pd.get_dummies(df['especie'], prefix='especie')
+
+    df = pd.concat([df, dummies], axis=1)
+
+
+    # AÑOS CLIENTE
+
+    logging.info("Calculando años_cliente...")
+
+    df['fecha_consulta'] = pd.to_datetime(
+        df['fecha_consulta'],
+        format='%d-%m-%Y',
+        errors='coerce'
+    )
+
+    primera_visita = df.groupby('dueño_email')['fecha_consulta'].transform('min')
+
+    fecha_actual = pd.Timestamp.today()
+
+    df['años_cliente'] = (
+        (fecha_actual - primera_visita).dt.days / 365
+    ).round(1)
+
+
+    # FORMATO FECHA
+
+    logging.info("Formateando fechas...")
+
+    df['fecha_consulta'] = df['fecha_consulta'].dt.strftime('%d-%m-%Y')
+
+
+    # EXPORTAR
+
+    logging.info("Exportando dataset limpio...")
+
+    df.to_csv(output_path, index=False)
+
+    logging.info("Proceso terminado correctamente.")
 
 
 # Ordenar 
-
 def ordenar_csv(file_path, output_path):
 
     df = pd.read_csv(file_path)
     df = df.map(lambda x: x.lower() if isinstance(x, str) else x)
 
     df_ordenado = df.sort_values(by='id_mascota')
+   
+    # Borra la unica fila que no tiene nada mas que el id
+    df_ordenado = df_ordenado.dropna(subset=['nombre','especie','raza','edad_años','peso_kg','fecha_consulta','dueño_nombre','dueño_email','motivo_consulta','costo_consulta'], how='all')
+    
+    df_ordenado = df_ordenado.fillna('No_Info')
+
+    df_ordenado['especie'] = df_ordenado['especie'].str.strip().replace({
+        'cat': 'gato',
+        'gata': 'gato',
+        'dog': 'perro',
+        'perra': 'perro'
+    })
     
     # Guardar en nuevo archivo CSV
     df_ordenado.to_csv(output_path, index=False)
 
+
+
 # ordenar_csv('./data/raw/mascotas.csv', './data/processed/datos_ordenados.csv')
     
+def formato_csv(file_path, output_path):
+
+    df = pd.read_csv(file_path)
+
+    # Convertir la columna fecha_consulta a formato dd-mm-yyyy
+    df['fecha_consulta'] = pd.to_datetime(df['fecha_consulta'], errors='coerce').dt.strftime('%d-%m-%Y')
+    
+
+    # Guardar el DataFrame con el formato corregido en un nuevo archivo CSV
+    df.to_csv(output_path, index=False)
+
+    # tabular cada dato y nombre columna en minuscula y sin espacios al principio ni al final
+    df.columns = df.columns.str.strip().str.lower()
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            df[col] = df[col].str.strip()
+
 
 def duplicados_csv(file_path, output_path):
 
     df = pd.read_csv(file_path)
 
-    # Eliminar filas duplicadas basándose en la columna id_mascota
-    df_sin_duplicados = df.drop_duplicates(subset='id_mascota')
+    # Eliminar filas duplicadas basándose en la columna dueño_email (se hace así ya que nadie puede tener el mismo email)
+    df_sin_duplicados = df.drop_duplicates(subset='dueño_email')
     
     # Guardar el DataFrame sin duplicados en un nuevo archivo CSV
     df_sin_duplicados.to_csv(output_path, index=False)
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import funciones as func
 
-func.ordenar_csv('./data/raw/mascotas.csv', './data/processed/datos_ordenados.csv')
+# funciones Antiguas
+# func.ordenar_csv('./data/raw/mascotas.csv', './data/processed/datos_ordenados.csv')
+
+# func.formato_csv('./data/processed/datos_ordenados.csv', './data/processed/datos_ordenados.csv')
+
+# func.duplicados_csv('./data/processed/datos_ordenados.csv', './data/processed/datos_sin_duplicados.csv')
+
+# Funcion Vigente
+func.procesar_csv('./data/raw/mascotas.csv','./data/processed/mascotas_final.csv')
